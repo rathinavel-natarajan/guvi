b8133481e4a60af7315330c40bf5c46cb86ede49
Rathinavel Natarajan <rathinavel1997n@gmail.com>
2017-07-20 15:08:03 +0530

    Update stringtointeger.c

--- stringtointeger.c
+++ stringtointeger.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
-
+#include<string.h>
 int main()
 {
-    char c;
-    scanf("%c",&c);
-    printf("%d",c);
+    char c[100];
+    gets(c);
+    int i,length=strlen(c);
+    for(i=0;i<length;i++){
+        printf("%d ",c[i]);
+    }
     return 0;
 }
