d9a046b03ddf96f8a3653cb91d35739301f31e54
Rathinavel Natarajan <rathinavel1997n@gmail.com>
2017-07-20 10:30:28 +0530

    Create positive,negativeorzero.c

---
+++ positive,negativeorzero.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int i;
+scanf("%d",&i);
+if(i>0)
+printf("number is positive");
+else if(i==0)
+printf("number is zero");
+else
+printf("number is negative");
+return 0;
+}
