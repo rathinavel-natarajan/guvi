7a284b009fdf517aaa6d3f1e157a50f09168035c
Rathinavel Natarajan <rathinavel1997n@gmail.com>
2017-09-25 15:16:40 +0530

    Create pro2.c

---
+++ pro2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,x=0,n,digi,a[10],temp,fin=0;
+    scanf("%d %d",n,digi);
+    while(n!=0){
+        a[x]=n%10;
+        n/=10;
+        x++;
+    }
+    for(i=0;i<x;i++){
+        for(j=0;j<x;j++){
+            if(a[i]>a[j]){
+                temp=a[j];
+                a[j]=a[i];
+                a[i]=temp;
+            }
+        }
+    }
+    x=x-digi;
+    for(i=0;i<x;i++){
+        fin=10*fin+a[i];
+    }
+    printf("%d",fin);
+    return 0;
+}
